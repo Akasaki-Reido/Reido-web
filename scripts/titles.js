5fe0cbc23dd02f7642e64e19db015b09e3920269
第一魔王-赤咲レイド <110758156+Akasaki-Reido@users.noreply.github.com>
2022-08-15 19:51:14 +0800

    Add popup js

--- scripts/titles.js
+++ scripts/titles.js
@@ -1,8 +1,23 @@
+var click_count = 0
+//var test = document.querySelector('h1')
+var popup_ = document.querySelector('dialog.popup')
+var close_ = document.querySelector('button.close-b')
 function presstitle() {
-    document.querySelector('h1').onclick = function() {
-        alert('触らない！\n吾輩は魔王だ！！！')
+    document.querySelector('h1.big-title').onclick = function() {
+        click_count += 1;
+        alert('吾輩は魔王だから！\n触らない！('+click_count+')');
+        //test code
+        //test.innerHTML = '赤咲レイド' + click_count;
+        if (click_count === 10) {
+            popup_.showModal();
+            click_count = 0;
+        };
     };
     return presstitle();
 };
+
+close_.addEventListener("click", function() {
+    popup_.close();
+});
 
 presstitle()
