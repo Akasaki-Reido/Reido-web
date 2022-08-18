629aea718b58ecfb3391f06608f7b7f3bacbb4f0
第一魔王-赤咲レイド <110758156+Akasaki-Reido@users.noreply.github.com>
2022-08-18 15:03:30 +0800

    Change how hidden words appear

--- scripts/blacktext.js
+++ scripts/blacktext.js
@@ -1,9 +1,11 @@
 var mytext = document.querySelector('span.word-block');
 
 mytext.onmouseover = function() {
-    mytext.style.backgroundColor = "#FFFFFF";
+    //mytext.style.backgroundColor = "#FFFFFF";
+    mytext.style.color = "#FFFFFF";
 }
 
 mytext.onmouseout = function() {
-    mytext.style.backgroundColor = "#000000";
+    //mytext.style.backgroundColor = "#000000";
+    mytext.style.color = "#000000";
 }
